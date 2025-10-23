fd9bfceb1e448953fd1c723779b0cdbc7f9f7c68
tableros <40235617+yevea@users.noreply.github.com>
2025-10-23 19:17:09 +0200

    Create config.php

---
+++ assets/config.php
@@ -0,0 +1,7 @@
+<?php
+// assets/config.php
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51S6EIpEKnTcZwfwY4sD8Ic8iW1PP5w95N4TWKINFpNLaEzgZZAolFO6VQYNg9vqYYyCodsScXkZGKx7rPnnFcNj000nByIfFSS');
+define('STRIPE_SECRET_KEY', 'sk_test_51S6EIpEKnTcZwfwYI79DEJigKAepTH0EwnRvlF2y33JiLJA00ApFEUSvBhjSDhkKyONYXVeyHNRs01HrNFWGXjCz00kmlluTqM');
+define('CURRENCY', 'eur'); // Adjust if needed
+define('MIN_ORDER_AMOUNT', 17500); // 175 EUR in cents for validation
+?>
